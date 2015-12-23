f587c86673d8c33283272254b947911320d7c612
Max Stemplevski <smt@joomlalms.com>
2015-12-23 21:55:23 +0300

    Update/ Added new sample #3 for real bad WSO shell

--- example.php
+++ example.php
@@ -12,3 +12,8 @@ $ex_code_scan->showResultTable();           // show result table (optional)
 $ex_code_scan = new BadCodeScan;            // create new object of BadCodeScan class (optional: you can collect multiple results by other masks)
 $ex_code_scan->searchBadCode( '.',  'system(',  '*.txt' );  // search bad code by custom mask and parameters.
 $ex_code_scan->showResultTable();           // show result table (optional)
+
+#Sample #3
+$ex_code_scan = new BadCodeScan;
+$ex_code_scan->searchBadCode( '.', ',""); /\*'); 
+$ex_code_scan->showResultTable(); 
